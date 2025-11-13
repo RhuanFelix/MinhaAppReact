be333e2bfede4ad4edbe98f7ca399a00dcf8ed17
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2025-11-13 14:43:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './views/Home';
+
+function App() {
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
