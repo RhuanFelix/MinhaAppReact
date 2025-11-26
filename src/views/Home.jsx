83ce0ef3a308570133db7d763ba6eb4d9262a247
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2025-11-26 08:38:39 -0300

    refactor: altera os caminhos dos imports

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,13 @@
-import Header from "../components/Header"
-import Main from "../components/Main"
-import Footer from "../components/Footer"
+import Header from "../components/header/Header"
+import Main from "../components/main/Main"
+import Footer from "../components/footer/Footer"
 
 const Home = () => {
   return (
     <>
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   )
 }
