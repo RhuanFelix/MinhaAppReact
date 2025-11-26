a8e7ba869f8687bcb8b5f86ab45f984d12d12464
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2025-11-26 08:39:05 -0300

    feat: adiciona o arquivo censoEscolar.js

---
+++ src/datasets/censoEscolar.js
@@ -0,0 +1,28 @@
+let instituicoesEnsino = [
+    {
+        nome:"IFPB - Guarabira",
+        municipio:"Guarabira",
+        estado:"Paraíba",
+        regiao:"Nordeste",
+        codigo:"1166",
+        urlImagem:"https://th.bing.com/th/id/R.965d4d025cc6fcd065630616e660ae09?rik=%2fqio%2fdgqFHpU8g&pid=ImgRaw&r=0"
+    },
+    {
+        nome:"IFPB - Campina Grande",
+        municipio:"Campina Grande",
+        estado:"Paraíba",
+        regiao:"Nordeste",
+        codigo:"1167",
+        urlImagem:"https://tse2.mm.bing.net/th/id/OIP.K6UBuPbzkzIb48YAT8BtugHaE6?rs=1&pid=ImgDetMain&o=7&rm=3"
+    },
+    {
+        nome:"IFPB - Esperança",
+        municipio:"Esperança",
+        estado:"Paraíba",
+        regiao:"Nordeste",
+        codigo:"1167",
+        urlImagem:"https://lh3.googleusercontent.com/gps-cs-s/AG0ilSzIPbjmQAUQFCilUqUSV-z9ugv5pb43VMwF-G_n1bpMzN9yrCfZs-WAH7QuOo_7eMVG68JG8FsdqWqzGxc7GLToyoGkYAzMGX6xuyBrQvCpn4fp3K0uLuCSFZ9KQHyB25VD9bnO=s680-w680-h510-rw"
+    }
+];
+
+export default instituicoesEnsino;
