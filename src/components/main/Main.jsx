008e3ac95c40b735deee8f96c57448d1484c0dd7
Rhuan dos Santos Félix <rhuansantosfelix@gmail.com>
2025-12-03 20:51:09 -0300

    adição do useState

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,52 +1,28 @@
-import { Carousel, Container, Button, Card, Row, Col } from 'react-bootstrap';
-import instituicoesEnsino from "../../datasets/censoEscolar.js";
+import InstituicoesEnsinoRankingCard from "../InstituicoesEnsinoRankingCard/InstituicoesEnsinoRankingCard.jsx";
+import CarrosselImagens from "../CarrosselImagens/CarrosselImagens.jsx"
 import ranking from "../../datasets/matriculas_pb.json";
 import "./main.css";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 const Main = () => {
-  let instituicoesEnsinoJson = [...instituicoesEnsino];
-  console.log(instituicoesEnsinoJson)
+
+  let [incrementar, setIncrementar] = useState(0);
+
+  const incrementarHandleClick = () => {
+    setIncrementar(++incrementar);
+  }
   return (
     <main>
-
       {/* Carrossel de imagens */}
-
-      <Carousel>
-        <Carousel.Item>
-          <div className="imagem-carrossel img1"></div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="imagem-carrossel img2"></div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className="imagem-carrossel img3"></div>
-        </Carousel.Item>
-      </Carousel>
+      <CarrosselImagens/>
 
       {/* Cartões da IE */}
+      <InstituicoesEnsinoRankingCard ranking={ranking}/>
 
-      <Container className="Cards-instituicoes">
-        <Row>
-          {ranking.map((instituicaoEnsino) => {
-            return (
-              <Col>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src={`/public/${instituicaoEnsino.CO_ENTIDADE}.jpg`} style={{height:'189.45px'}} />
-                  <Card.Body>
-                    <Card.Title>{instituicaoEnsino.NO_ENTIDADE}</Card.Title>
-                    <Card.Text>
-                      Município: {instituicaoEnsino.NO_MUNICIPIO}<br/>
-                      Estado: {instituicaoEnsino.NO_UF}<br/>
-                      Nº matrículas do ensino básico:{instituicaoEnsino.QT_MAT_BAS}
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )})}
-        </Row>
-      </Container>
-      
+      {incrementar}<br/>
+
+      <Button onClick={incrementarHandleClick}>Adicionar</Button>
     </main>
   )
 }
